3a8fbc0bff69f42501e16588eb71b40298b80a79
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-09 13:12:28 +0600

    chore: run tests and prettier

--- typescript/tests/AuctionMessageTranslator.test.ts
+++ typescript/tests/AuctionMessageTranslator.test.ts
@@ -6,8 +6,7 @@ describe('AuctionMessageTranslator tests', () => {
     });
 
     it('should notify bid details when price message received', () => {
-        const message =
-            'SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;';
+        const message = 'SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;';
         // TODO: write a test for this message translation
         expect(1).toEqual(2);
     });
