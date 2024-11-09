32c639a2b4ae59ad8a0bd4af2b88c15ee23581eb
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-09 13:03:10 +0600

    feat: add source files

---
+++ typescript/src/AuctionMessageTranslator.ts
@@ -0,0 +1,32 @@
+import {AuctionEventListener} from "./AuctionEventListener";
+
+export class AuctionMessageTranslator {
+    private readonly listener: AuctionEventListener;
+
+    constructor(listener: AuctionEventListener) {
+        this.listener = listener;
+    }
+
+    public processMessage(message: string) {
+        if (message.includes('CLOSE')) {
+            this.listener.auctionClosed();
+        } else if (message.includes('PRICE')) {
+            const data: { [key: string]: string } = {};
+            message
+                .split(';')
+                .filter((element) => element.includes(':'))
+                .forEach((element) => {
+                    const pair = element.split(':');
+                    data[pair[0].trim()] = pair[1].trim();
+                });
+
+            const currentPrice = parseInt(data["CurrentPrice"]);
+            const increment = parseInt(data["Increment"]);
+            const bidder = data["Bidder"];
+
+            // bug: should notify listener
+        } else {
+            // bug: should notify listener
+        }
+    }
+}
