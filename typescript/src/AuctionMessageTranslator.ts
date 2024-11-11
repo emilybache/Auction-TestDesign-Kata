3fe15e1ceb56516c686d1ead37534cdb862a164d
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-11 13:32:49 +0600

    refactor: extract parseMessageData()

--- typescript/src/AuctionMessageTranslator.ts
+++ typescript/src/AuctionMessageTranslator.ts
@@ -11,14 +11,7 @@ export class AuctionMessageTranslator {
         if (message.includes('CLOSE')) {
             // bug: should notify listener
         } else if (message.includes('PRICE')) {
-            const data: { [key: string]: string } = {};
-            message
-                .split(';')
-                .filter((element) => element.includes(':'))
-                .forEach((element) => {
-                    const pair = element.split(':');
-                    data[pair[0].trim()] = pair[1].trim();
-                });
+            const data = this.parseMessageData(message);
 
             const currentPrice = parseInt(data['CurrentPrice']);
             const increment = parseInt(data['Increment']);
@@ -28,5 +21,17 @@ export class AuctionMessageTranslator {
         } else {
             // bug: should notify listener
         }
+    }
+
+    private parseMessageData(message: string) {
+        const data: { [key: string]: string } = {};
+        message
+            .split(';')
+            .filter((element) => element.includes(':'))
+            .forEach((element) => {
+                const pair = element.split(':');
+                data[pair[0].trim()] = pair[1].trim();
+            });
+        return data;
     }
 }
